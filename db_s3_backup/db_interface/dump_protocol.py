53498c0da9c276204885c106d7189347fe89bacd
Bahattin Cinic <bahattin@adphorus.com>
2018-01-04 21:24:13 +0300

    use `NotImplementedError` instead of `ValueError`

--- db_s3_backup/db_interface/dump_protocol.py
+++ db_s3_backup/db_interface/dump_protocol.py
@@ -3,5 +3,6 @@ from exceptions import ValueError
 
 class DumpProtocol(object):
 
-    def dump(self, config, s3_bucket, s3_bucket_key_name, filepath, verbose=False, upload_callback=None):
-        raise ValueError('DumpProtocol not followed')
+    def dump(self, config, s3_bucket, s3_bucket_key_name,
+             filepath, verbose=False, upload_callback=None):
+        raise NotImplementedError('dump not implemented by {0}'.format(self))
