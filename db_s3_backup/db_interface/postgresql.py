01b0c76dca3cbd28c800580e7f1418c718058448
Bahattin Cinic <bahattin@adphorus.com>
2018-01-04 22:11:26 +0300

    Fix postgresql adapter

--- db_s3_backup/db_interface/postgresql.py
+++ db_s3_backup/db_interface/postgresql.py
@@ -13,26 +13,17 @@ class PostgreSQLDump(BaseDump):
             '-d', config['NAME'],
             '-h', config['HOST'],
             '-p', config['PORT'],
-            '-u', config['USER']
+            '-u', config['USER'],
+            '> ', filepath
         ]
-        proc = subprocess.Popen(sqldump_cmd, stdout=subprocess.PIPE)
 
         if verbose:
             print('Dumping PostgreSQL database: {database} to file {filepath}'.format(
                 database=config['NAME'], filepath=filepath))
 
-        with open(filepath, 'w+') as f:
-            while True:
-                buf = proc.stdout.read(4096 * 1024)  # Read 4 MB
-                if buf != '':
-                    f.write(buf)
-                    if verbose:
-                        print('- Written 4 MB')
-                else:
-                    break
+        process = subprocess.Popen(sqldump_cmd, stdout=subprocess.PIPE)
+        process.communicate()
 
-            if verbose:
-                print('+ Dump finished')
-
-            if upload_callback is not None:
+        if upload_callback is not None:
+            with open(filepath, 'w+') as f:
                 upload_callback(f, s3_bucket, s3_bucket_key_name, verbose)
